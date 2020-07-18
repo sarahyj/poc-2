e44f091549da44618c6b01ffab643f103931a55e
sarahyj <s.aljindan@hungerstation.com>
2020-07-18 19:37:53 +0300

    text

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Plugin Two</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
